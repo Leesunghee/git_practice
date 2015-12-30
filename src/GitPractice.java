10fb9b64ad66cee4c2bd65d89e95abff649eef54
realwunz <realwunz@gmail.com>
2015-12-30 13:51:09 +0900

    modify content of GitPratice.java file by branch

--- src/GitPractice.java
+++ src/GitPractice.java
@@ -1,7 +1,7 @@
 /**
  * Created by 0915183 on 2015-12-30.
  */
-public class GitPratice {
+public class GitPractice {
 
     public static void main(String[] args) {
         System.out.println("Hello Git World");
